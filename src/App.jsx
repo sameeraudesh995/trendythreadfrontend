0dc6074ac86bfd68a7fb2995e38736c25c1acad9
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2023-08-10 20:24:13 +0530

    carousel added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './customer/components/Navigation/Navigation';
+import HomePage from './customer/pages/HomePage/HomePage';
 
 function App() {
   return (
     <div className="">
       <Navigation/>
    <div>
-    home
+   <HomePage/>
+    
    </div>
     </div>
   );
