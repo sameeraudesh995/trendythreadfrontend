96e0322fbc9ba8edc0564047366ddba9fd44565c
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2023-08-30 18:58:22 +0530

    Authmodal Created

---
+++ src/customer/Auth/AuthModal.jsx
@@ -0,0 +1,36 @@
+import { Box, Modal, Typography } from '@mui/material'
+import React from 'react'
+import RegisterForm from './RegisterForm';
+
+
+const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 500,
+    bgcolor: "background.paper",
+    boxShadow: 24,
+    p: 4,
+  };
+
+const AuthModal = ({handleClose, open}) => {
+  return (
+    <div>
+        <Modal
+  open={open}
+  onClose={handleClose}
+  aria-labelledby="modal-modal-title"
+  aria-describedby="modal-modal-description"
+>
+  <Box sx={style}>
+    
+   <RegisterForm/>
+
+  </Box>
+</Modal>
+    </div>
+  )
+}
+
+export default AuthModal
